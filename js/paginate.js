39b68e830f12533f47db0a392f308d2b4f56c081
dunnbro <dunbri@gmail.com>
2015-03-25 13:36:15 -0400

    pagination
    displays one chapter at a time and adds chapter navigation buttons

---
+++ js/paginate.js
@@ -0,0 +1,141 @@
+var paginate = {};
+
+	paginate.init = function(){
+		displaySetup();
+		loadButtons();
+		clickEvents();
+		checkPage();
+	}	
+		
+	function loadButtons(){
+
+		$("#story")
+			.after('<div id="js-buttons">');
+		$('#js-buttons')
+			.append('<input type="button" value="First" id="js-first">')
+			.append('<input type="button" value="Prev" id="js-previous">')
+			.append('<span id="js-pagecount"></span>')
+			.append('<input type="button" value="Next" id="js-next">')
+			.append('<input type="button" value="Last" id="js-last">');
+				
+		updatePageNumber();
+	}
+
+	function clickEvents(){
+		$('#js-next').on('click', nextPage);
+		$('#js-previous').on('click', prevPage);
+		$('#js-first').on('click', firstPage);
+		$('#js-last').on('click', lastPage);
+	}
+
+	function checkPage(){
+
+		var finalPage = (parseInt($('.js-currentchapter').index())+1);
+		var numPages = parseInt($('#story').children().size());
+		
+		if($('.js-currentchapter').index()=== 0){
+			$('#js-first').prop('disabled', true);
+			$('#js-previous').prop('disabled', true);
+			$('#js-last').prop('disabled', false);
+			$('#js-next').prop('disabled', false);
+			
+		}
+		
+		else if(finalPage === numPages){
+			$('#js-last').prop('disabled', true);
+			$('#js-next').prop('disabled', true);
+			$('#js-first').prop('disabled', false);
+			$('#js-previous').prop('disabled', false);
+		}
+		
+		else {
+			$('#js-first').prop('disabled', false);
+			$('#js-last').prop('disabled', false);
+			$('#js-previous').prop('disabled', false);
+			$('#js-next').prop('disabled', false);
+		}
+		
+	}
+
+	function updatePageNumber(){
+
+		var numPages = parseInt($('#story').children().size());
+		var pageNumber = parseInt($('.js-currentchapter').index()+1);
+		$("#js-pagecount").text("Chapter" + " " + pageNumber + " " + "of" + " " + numPages);
+
+	}
+
+	function nextPage (){
+		
+		var currentPage = $(".js-currentchapter");
+		var nextPage = currentPage.next();
+		
+		currentPage
+			.hide()
+			.removeClass('js-currentchapter');
+		nextPage
+			.show()
+			.addClass('js-currentchapter');
+			
+		updatePageNumber();
+		checkPage();
+		
+	}
+
+	function prevPage(){
+
+		var currentPage = $(".js-currentchapter");
+		var prevPage = currentPage.prev();
+		
+		currentPage
+			.hide()
+			.removeClass('js-currentchapter');
+		prevPage
+			.show()
+			.addClass('js-currentchapter');
+			
+		updatePageNumber();
+		checkPage();
+		
+	}
+
+	function firstPage(){
+		var currentPage = $(".js-currentchapter");
+		currentPage
+			.hide()
+			.removeClass('js-currentchapter');
+		$(".js-storySection").first()
+			.show()
+			.addClass('js-currentchapter');
+			
+		updatePageNumber();
+		checkPage();
+		
+	}
+
+	function lastPage(){
+		var currentPage = $(".js-currentchapter");
+		currentPage
+			.hide()
+			.removeClass('js-currentchapter');
+		
+		$(".js-storySection").last()
+			.show()
+			.addClass('js-currentchapter');
+			
+		updatePageNumber();
+		checkPage();
+	}
+
+
+	function displaySetup() {
+		$(".js-storySection")
+			.hide();
+		$(".js-storySection").first().addClass('js-currentchapter');
+		$(".js-currentchapter").show();
+	}	
+		
+paginate.init();	
+	
+
+
