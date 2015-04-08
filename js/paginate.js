ea0388f4764b00e5ec48a58e0df2f00472c5bb73
dunnbro <dunbri@gmail.com>
2015-04-07 23:28:22 -0400

    condensed disabled buttons properties, fixed hash on page load issue
    issues #10 and #11

--- js/paginate.js
+++ js/paginate.js
@@ -11,18 +11,17 @@
 
     paginate.init = function() {
         paginate.numPages = ($('#story').children().length);
-		paginate.sectionTerm = 'Part';
+        paginate.sectionTerm = 'Part';
+        paginate.addLocationHash();
         paginate.loadButtons();
         paginate.displaySetup();
         paginate.checkPage();
         paginate.buttons();
-        paginate.addLocationHash();
         paginate.eventListener();
-        window.location.hash = '';
-	};
+    };
 
     paginate.loadButtons = function() {
-      var buttons =
+        var buttons =
             '<div id="js-buttons" class="pag-nav">' +
             '<span class="pag-btnGroup pag-btnGroup--1">' +
             '<input type="button" value="First" id="js-first" class="pag-first">' +
@@ -41,44 +40,36 @@
         $('#story')
             .after(buttons);
         paginate.updatePageNumber();
-    }
+    };
 
     paginate.checkPage = function() {
-		var $finalPage = ($('.js-currentchapter').index() + 1);
+        var $finalPage = ($('.js-currentchapter').index() + 1);
 
         if ($('.js-currentchapter').index() === 0) {
-            $('#js-first').prop('disabled', true);
-            $('#js-previous').prop('disabled', true);
-            $('#js-last').prop('disabled', false);
-            $('#js-next').prop('disabled', false);
-
+            $('#js-first, #js-previous').prop('disabled', true);
+            $('#js-last, #js-next').prop('disabled', false);
         } else if ($finalPage === paginate.numPages) {
-            $('#js-last').prop('disabled', true);
-            $('#js-next').prop('disabled', true);
-            $('#js-first').prop('disabled', false);
-            $('#js-previous').prop('disabled', false);
+            $('#js-last, #js-next').prop('disabled', true);
+            $('#js-first, #js-previous').prop('disabled', false);
         } else {
-            $('#js-first').prop('disabled', false);
-            $('#js-last').prop('disabled', false);
-            $('#js-previous').prop('disabled', false);
-            $('#js-next').prop('disabled', false);
+            $('#js-first, #js-last, #js-previous, #js-next').prop('disabled', false);
         }
-    }
+    };
 
     paginate.updatePageNumber = function() {
-		//updates page number in #js-pagecount
+        //updates page number in #js-pagecount
         var $pageNumber = ($('.js-currentchapter').index() + 1);
         $('#js-pagecount').text($pageNumber);
-    }
+    };
 
     paginate.updateHash = function() {
         var $pageNumber = ($('.js-currentchapter').index() + 1);
         window.location.hash = 'section' + $pageNumber;
-    }
+    };
 
     paginate.buttons = function() {
         $('#js-buttons').on('click', 'input', function(event) {
-            var $target = ($(event.target));
+            //var $target = ($(event.target));
             var $currentPage = $('.js-currentchapter');
             var $firstPage = $('.js-storySection').first();
             var $previousPage = $currentPage.prev();
@@ -86,47 +77,55 @@
             var $lastPage = $('.js-storySection').last();
 
             $currentPage.removeClass('js-currentchapter');
-			switch (event.target.id) {
-				case 'js-first':
-					$firstPage.addClass('js-currentchapter');
-					break;
+            switch (event.target.id) {
+                case 'js-first':
+                    $firstPage.addClass('js-currentchapter');
+                    break;
 
-				case 'js-previous':
-					$previousPage.addClass('js-currentchapter');
-					break;
+                case 'js-previous':
+                    $previousPage.addClass('js-currentchapter');
+                    break;
 
-				case 'js-next':
-					$followingPage.addClass('js-currentchapter');
-					break;
+                case 'js-next':
+                    $followingPage.addClass('js-currentchapter');
+                    break;
 
-				case 'js-last':
-					$lastPage.addClass('js-currentchapter');
-					break;
-			}
+                case 'js-last':
+                    $lastPage.addClass('js-currentchapter');
+                    break;
+            }
 
-			paginate.updatePageNumber();
-			paginate.updateHash();
-			paginate.checkPage();
+            paginate.updatePageNumber();
+            paginate.updateHash();
+            paginate.checkPage();
         });
-    }
+    };
 
 
     paginate.displaySetup = function() {
-        $('.js-storySection').first().addClass('js-currentchapter');
-        paginate.updatePageNumber();
-    }
+        //on page load, if no location.hash exists, simply show the first story section
+        if (!window.location.hash) {
+            $('.js-storySection').first().addClass('js-currentchapter');
+            paginate.updatePageNumber();
+        } else {
+            //if location hash is already present on page load, show the appropriate story section
+            var validHash = window.location.hash.replace('#section', '');
+            $('#section' + validHash).addClass('js-currentchapter');
+            paginate.updatePageNumber();
+        }
+    };
 
     paginate.addLocationHash = function() {
-		//adds numbered section id to each storySection
+        //adds numbered section id to each storySection
         $('.js-storySection').each(function(i) {
             var hashValue = 'section' + (i++ + 1);
             $(this).prop('id', hashValue);
         });
-    }
+    };
 
     paginate.eventListener = function() {
-		//on window.location.hash value change, checks that any typed hash value is valid, then displays the appropriate section
-        $(window).on('hashchange', function(e) {
+        //on window.location.hash value change, checks that any typed hash value is valid, then displays the appropriate section
+        $(window).on('hashchange', function() {
             var validHash = window.location.hash.replace('#section', '');
             if (validHash >= 1 && validHash <= paginate.numPages) {
                 var $currentPage = $('.js-currentchapter');
@@ -137,7 +136,7 @@
                 paginate.checkPage();
             }
         });
-    }
+    };
 
     paginate.init();
 });
