4403dab2339ec42954a7aeebb7d466080e7ed5b5
David Wilhelm <davwilh@gmail.com>
2015-04-21 14:37:29 -0400

    file clean up, fixed issue with jQuery url

--- src/paginate.js
+++ src/paginate.js
@@ -1,71 +1,31 @@
-/*!
- * jQuery lightweight plugin boilerplate
- * Original author: @ajpiano
- * Further changes, comments: @addyosmani
- * Licensed under the MIT license
- */
-
-// the semi-colon before the function invocation is a safety
-// net against concatenated scripts and/or other plugins
-// that are not closed properly.
-;(function ( $, window, document, undefined ) {
-
-    // undefined is used here as the undefined global
-    // variable in ECMAScript 3 and is mutable (i.e. it can
-    // be changed by someone else). undefined isn't really
-    // being passed in so we can ensure that its value is
-    // truly undefined. In ES5, undefined can no longer be
-    // modified.
-
-    // window and document are passed through as local
-    // variables rather than as globals, because this (slightly)
-    // quickens the resolution process and can be more
-    // efficiently minified (especially when both are
-    // regularly referenced in your plugin).
-
+;(function ($, window, document, undefined) {
     // Create the defaults once
-    var pluginName = "paginationplugin",
-        defaults = {
-            sectionTerm: 'Page'
-			
-        };
-		
+    var pluginName = 'paginate';
+    var defaults = {
+        sectionTerm: 'Page'
+    };
 
     // The actual plugin constructor
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
 		self = this;
 		this.element = element;
-        // jQuery has an extend method that merges the
-        // contents of two or more objects, storing the
-        // result in the first object. The first object
-        // is generally empty because we don't want to alter
-        // the default options for future instances of the plugin
-        this.options = $.extend( {}, defaults, options) ;
+        this.options = $.extend({}, defaults, options);
 		this._defaults = defaults;
         this._name = pluginName;
 		this.init();
     }
 
     Plugin.prototype = {
-
         init: function() {
 			this.numPages = $('#story').children().length;
-			//this.sectionTerm = 'Part';
 			this.addLocationHash();
 			this.loadButtons();
 			this.displaySetup();
 			this.checkPage();
 			$('#js-buttons').on('click', 'input', this.buttons);
 			$(window).on('hashchange', this.hashListener);
-			
-            // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.options
-            // you can add more functions like the one below and
-            // call them like so: this.yourOtherFunction(this.element, this.options).
         },
-		
+
 		addLocationHash: function() {
 			//adds numbered section id to each storySection
 			$('.js-storySection').each(function(i) {
@@ -89,11 +49,11 @@
 			'<input type="button" value="Last" id="js-last" class="pag-last">' +
 			'</span>' +
 			'</div>';
-			
+
 			$('#story').after(buttons);
 			this.updatePageNumber();
 		},
-		
+
 		displaySetup: function() {
 			//on page load, if no location.hash exists, simply show the first story section
 			if (!window.location.hash) {
@@ -106,9 +66,9 @@
 				this.updatePageNumber();
 			}
 		},
-		
+
 		checkPage: function() {
-			//all buttons enabled unless it's the first or last page 
+			//all buttons enabled unless it's the first or last page
 			$('#js-buttons input').prop('disabled', false);
 			if ($('.js-currentchapter').index() === 0) {
 				$('#js-first, #js-previous').prop('disabled', true);
@@ -117,7 +77,7 @@
 				$('#js-last, #js-next').prop('disabled', true);
 			}
 		},
-		
+
 		buttons: function(event) {
 			var $currentPage = $('.js-currentchapter');
 			var $section = $('.js-storySection');
@@ -133,11 +93,12 @@
 			self.updatePageNumber();
 			self.updateHash();
 			self.checkPage();
-			
+
 		},
-		
+
 		hashListener: function() {
-			//on window.location.hash value change, checks that any typed hash value is valid, then displays the appropriate section
+			//on window.location.hash value change,
+            //checks that any typed hash value is valid, then displays the appropriate section
 			var validHash = window.location.hash.replace('#section', '');
 			if (validHash >= 1 && validHash <= self.numPages) {
 				$('.js-currentchapter').removeClass('js-currentchapter');
@@ -147,12 +108,12 @@
 				self.checkPage();
 			}
 		},
-		
+
 		updatePageNumber: function() {
 			//updates page number in #js-pagecount
 			$('#js-pagecount').text($('.js-currentchapter').index() + 1);
 		},
-		
+
 		updateHash: function() {
 			window.location.hash = 'section' + ($('.js-currentchapter').index() + 1);
 		}
@@ -160,13 +121,13 @@
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName,
-                new Plugin( this, options ));
+    $.fn[pluginName] = function (options) {
+        return this.each(function() {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                new Plugin(this, options));
             }
         });
     };
 
-})( jQuery, window, document );
+})(jQuery, window, document);
